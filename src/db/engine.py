eb5fcadf1ab247de626ab4387892ca3bf051cbbf
g-tuyub <g.a.tuyub@gmail.com>
2025-11-30 14:44:34 -0600

    refactoring change on get_engine

--- src/db/engine.py
+++ src/db/engine.py
@@ -1,6 +1,5 @@
-from sqlalchemy import create_engine, Engine
+from sqlalchemy import create_engine, Engine, URL
 
-
-def get_engine(connection_string: str, fast_executemany: bool = True) -> Engine:
-    engine = create_engine(connection_string, fast_executemany=fast_executemany)
+def get_engine(connection_url: URL, fast_executemany: bool = True) -> Engine:
+    engine = create_engine(connection_url, fast_executemany=fast_executemany)
     return engine
