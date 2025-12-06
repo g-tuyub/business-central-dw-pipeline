85a3c6f4b5dfe8189a40b8ac449ac8ffa411c454
g-tuyub <g.a.tuyub@gmail.com>
2025-12-06 13:55:02 -0600

    completed implementation of deploy_db script

--- scripts/deploy_db.py
+++ scripts/deploy_db.py
@@ -1,8 +1,27 @@
 from bcsync.db.engine import create_engine
 from bcsync.config.config import  Config
+from bcsync.db.models.base import Base
+from bcsync.db.models import staging, core
+import sys
+import logging
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def deploy_db() -> None:
-    config = Config.from_env()
-    engine = create_engine(config.db.connection_string)
+    try:
+        config = Config.from_env()
+        engine = create_engine(config.db.connection_string)
+        logger.info(f"Connected to {config.db.connection_string}")
 
+        Base.metadata.create_all(engine)
+
+        logger.info(f"SUCCESFUL db deployment!")
+
+    except Exception as e:
+        logger.error(f"FAILED db deployment!, error : {e}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    deploy_db()
