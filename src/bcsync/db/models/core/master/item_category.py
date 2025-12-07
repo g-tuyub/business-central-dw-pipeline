14abcc65208e4363bda27bf4414f99aa01e07c9f
g-tuyub <g.a.tuyub@gmail.com>
2025-12-07 13:07:03 -0600

    removed unnecessary column

--- src/bcsync/db/models/core/master/item_category.py
+++ src/bcsync/db/models/core/master/item_category.py
@@ -9,6 +9,5 @@ class ItemCategory(CoreBase):
 
     code: Mapped[str] = mapped_column(String(20), nullable=False)
     name: Mapped[str] = mapped_column(String(100), nullable=True)
-    parent_category_id: Mapped[int] = mapped_column(Integer, ForeignKey(f'{DBSchemas.CORE}.item_category.id'), nullable=True,index=True)
     parent_category_code: Mapped[str] = mapped_column(String(20), nullable=True)
     has_children: Mapped[bool] = mapped_column(Boolean)
